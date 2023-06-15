c3de7bc0d2593484d73097e046999eb5a16b1daf
Vritansh Kamal <vritansh14@gmail.com>
2023-06-14 23:38:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
 
 from flask import Flask, request
 import pandas as pd
@@ -17,9 +15,10 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import xgboost as xgb
 import joblib
+import openai
+from dotenv import load_dotenv, find_dotenv
+import os
 
-
-# In[5]:
 
 
 #Imports for sentiment analysis 
@@ -30,18 +29,22 @@ from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-# In[6]:
 
 app = Flask(__name__)
 
 loaded_model = joblib.load('customer_behavior_model.pkl')
 
-# API keyws that yous saved earlier
+# API keys
+# Load the .env file
+load_dotenv(find_dotenv())
 
-# api_key = "insert yours"
-# api_secrets = "insert yours"
-# access_token = "insert yours"
-# access_secret = "insert yours"
+# Get the API key and secret
+api_key = os.environ.get("TWITTER_API_KEY")
+api_secrets = os.environ.get("TWITTER_API_SECRET")
+access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_secret = os.environ.get("TWITTER_ACCESS_SECRET")
+
+
  
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(api_key,api_secrets)
@@ -54,75 +57,70 @@ except:
     print('Failed authentication')
 
 
-# In[8]:
-
 
 def percentage(part,whole):
      return 100 * float(part)/float(whole)
 
 def fetch_top_user_tweets():
-            keyword = "tesla"
-            noOfTweet = 10
-            # keyword = input('Please enter keyword or hashtag to search: ')
-            # noOfTweet = int(input ('Please enter how many tweets to analyze: '))
-            tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(noOfTweet)
-            positive = 0
-            negative = 0
-            neutral = 0
-            polarity = 0
-            tweet_list = []
-            neutral_list = []
-            negative_list = []
-            positive_list = []
-            positive_user = []
-            for tweet in tweets: 
-              #print(tweet.text)
-              tweet_list.append(tweet.text)
-              analysis = TextBlob(tweet.text)
-              score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
-              neg = score['neg']
-              neu = score['neu']
-              pos = score['pos']
-              comp = score['compound']
-              polarity += analysis.sentiment.polarity
+    keyword = "tesla"
+    noOfTweet = 10
+    # keyword = input('Please enter keyword or hashtag to search: ')
+    # noOfTweet = int(input ('Please enter how many tweets to analyze: '))
+    tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(noOfTweet)
+    positive = 0
+    negative = 0
+    neutral = 0
+    polarity = 0
+    tweet_list = []
+    neutral_list = []
+    negative_list = []
+    positive_list = []
+    positive_user = []
+    for tweet in tweets: 
+        #print(tweet.text)
+        tweet_list.append(tweet.text)
+        analysis = TextBlob(tweet.text)
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
+        neg = score['neg']
+        neu = score['neu']
+        pos = score['pos']
+        comp = score['compound']
+        polarity += analysis.sentiment.polarity
 
-              if neg > pos:
-                negative_list.append(tweet.text)
-                negative += 1
-              elif pos > neg:
-                positive_list.append(tweet.text)
-                positive_user.append(tweet.user.id)
-                positive += 1
+        if neg > pos:
+            negative_list.append(tweet.text)
+            negative += 1
+        elif pos > neg:
+            positive_list.append(tweet.text)
+            positive_user.append(tweet.user.id)
+            positive += 1
 
-              elif pos == neg:
-                neutral_list.append(tweet.text)
-                neutral += 1
-            positive = percentage(positive, noOfTweet)
-            negative = percentage(negative, noOfTweet)
-            neutral = percentage(neutral, noOfTweet)
-            polarity = percentage(polarity, noOfTweet)
-            positive = format(positive, '.1f')
-            negative = format(negative, '.1f')
-            neutral = format(neutral, '.1f')
+        elif pos == neg:
+            neutral_list.append(tweet.text)
+            neutral += 1
             
-            count=0
-            dicts = {}
-            for user in positive_user:
-              count=count+1
-              user_tweet = []
-              timeline=tweepy.Cursor(api.user_timeline, id=user).items(5)
-              for r in timeline:
+    positive = percentage(positive, noOfTweet)
+    negative = percentage(negative, noOfTweet)
+    neutral = percentage(neutral, noOfTweet)
+    polarity = percentage(polarity, noOfTweet)
+    positive = format(positive, '.1f')
+    negative = format(negative, '.1f')
+    neutral = format(neutral, '.1f')
+    
+    count=0
+    dicts = {}
+    for user in positive_user:
+        count=count+1
+        user_tweet = []
+        timeline=tweepy.Cursor(api.user_timeline, id=user).items(5)
+        for r in timeline:
                 user_tweet.append(r.text)
                 #print(r.text)
-              dicts[user] = user_tweet
+        dicts[user] = user_tweet
 
 #               print("user {}".format(count))
+    return dicts
 
-
-            return dicts
-
-
-# In[9]:
 
 
 def make_prediction(new_data):
@@ -144,17 +142,51 @@ def make_prediction(new_data):
     return bool(predictions[0])  # Convert 0/1 to True/False
 
 
-# Sample data
-# {
-#     "Age": 30,
-#     "Gender": "Male",
-#     "Purchased": true,
-#     "EstimatedSalary": 200000,
-#     "Profession": "Entertainment",
-#     "Ever_Married": false,
-#     "Spending_Score": "High"
-# }
+#openai api key
+openai_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = openai_key
 
+def generate_customized_email(data):
+    '''
+    Using the variables in the data pandas dataframe, this function will return a customized email
+    The variables are used as a prompt in openAI's GPT-4 to generate the email
+    Input: data - pandas dataframe
+    '''
+
+    # There is only one row in the dataframe
+    row = data.iloc[0]
+
+    if row['Prediction'] == False:
+      return
+
+    prompt = """
+        Act like a professional sales email writer. The purpose is to sell a particular stock to customer.
+        The customer is a stock investor and you are trying to sell a particular stock to him/her.
+        Understand the customer behavior from the details and tweets provided.
+        The email should be personalized and should be written in a professional manner.
+        There should be some content about the stock and the company.
+        The email should be written in a way that the customer is convinced to buy the stock.
+        The email should contain the following details:\n
+    """
+    prompt += "Stock Name: " + row['Stock Name'] + "\n"
+    prompt += "Customer Age: " + str(row['Age']) + "\n"
+    prompt += "Customer Estimated Salary: " + str(row['EstimatedSalary']) + "\n"
+    prompt += "Customer Gender: " + str(row['Gender']) + "\n"
+    prompt += "Customer Profession: " + str(row['Profession']) + "\n"
+    prompt += "Customer Ever Married: " + str(row['Ever_Married']) + "\n"
+    prompt += "Customer Spending Score: " + str(row['Spending_Score']) + "\n"
+    prompt += "Customer Tweets: " + str(row['Tweets']) + "\n"
+
+    messages= [{'role': 'user', 'content': prompt}]
+    response = openai.ChatCompletion.create(
+        model = os.environ.get("OPENAI_MODEL"),
+        messages = messages,
+        temperature = 0
+    )
+    return response.choices[0].message["content"]
+
+        
+        
 @app.route('/generate', methods=['POST'])
 def predict():
     data = request.get_json()
@@ -172,13 +204,13 @@ def predict():
     twitterhandle = data.get('twitter_handle')
     
     new_data = pd.DataFrame({
-    'Age': [age],
-    'EstimatedSalary': [estimated_salary],
-    'Purchased': [purchased],
-    'Gender': [gender],
-    'Profession': [profession],
-    'Ever_Married': [Ever_Married],
-    'Spending_Score': [Spending_Score]
+        'Age': [age],
+        'EstimatedSalary': [estimated_salary],
+        'Purchased': [purchased],
+        'Gender': [gender],
+        'Profession': [profession],
+        'Ever_Married': [Ever_Married],
+        'Spending_Score': [Spending_Score]
     })
 
     
@@ -188,9 +220,20 @@ def predict():
     if(len(list(s.keys())) >0):
         first_user = list(s.keys())[0]
         tweet_1 = s[first_user][0]
-    print(tweet_1)
-    
-    return str(prediction)
+       
+    final_data = pd.DataFrame({
+        'Age': [age],
+        'EstimatedSalary': [estimated_salary],
+        'Purchased': [purchased],
+        'Gender': [gender],
+        'Profession': [profession],
+        'Ever_Married': [Ever_Married],
+        'Spending_Score': [Spending_Score],
+        'Prediction' : [prediction],
+        'Tweets' : [tweet_1]
+    })
+    email_gen = generate_customized_email(final_data)
+    return str(email_gen)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
